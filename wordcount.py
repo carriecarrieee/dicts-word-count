3b06a1b87a4c3c5085e4bbc33ea200be35203c79
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 12:40:24 -0700

    Prints dictionary of words with count

---
+++ wordcount.py
@@ -0,0 +1,24 @@
+# put your code here.
+
+# Opens file
+
+
+# Read each line in file
+
+
+def get_word_counts(text):
+    """Count letter occurrences. Return dict of {word:count}."""
+
+    text = open(text)
+    word_counts = {}
+
+    for line in text:
+        list_of_words = line.rstrip().split()
+
+        for word in list_of_words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+
+    print word_counts
+
+
+get_word_counts("test.txt")
